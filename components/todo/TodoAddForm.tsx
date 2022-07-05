d4eb4b654243bf3d1e8e7d39d0eab115aa070f83
Dominik <dominik.rubroeder@icloud.com>
2022-07-05 09:43:22 +0200

    CRUD – add todo when submitting TodoAddForm

--- components/todo/TodoAddForm.tsx
+++ components/todo/TodoAddForm.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ITodo } from '../../data';
+import { TodoContext } from '../../store/TodoContext';
 
 const TodoAddForm: React.FC = () => {
+  const todoCtx = useContext(TodoContext);
+  const [newTodoTitle, setNewTodoTitle] = useState<string>('');
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (newTodoTitle === '') return;
+
+    const newTodo: ITodo = {
+      id: (Date.now() + Math.random()).toString(),
+      completed: false,
+      title: newTodoTitle,
+    };
+
+    todoCtx?.addTodo(newTodo);
+
+    setNewTodoTitle('');
+  };
+
   return (
-    <form className="flex items-center gap-4 bg-app-tdark-blue-desaturated-800 p-4 rounded-md">
+    <form
+      className="flex items-center gap-4 bg-app-tdark-blue-desaturated-800 p-4 rounded-md"
+      onSubmit={(e) => onSubmitHandler(e)}
+    >
       <div className="w-4 h-4 border border-app-tdark-gray-blue-600 rounded-full transition-all"></div>
 
       <input
         type="text"
         placeholder="Create a new todo..."
         className="flex-1 bg-transparent focus:outline-none"
+        value={newTodoTitle}
+        onChange={(e) => setNewTodoTitle(e.target.value)}
       />
     </form>
   );
